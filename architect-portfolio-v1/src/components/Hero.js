730efb1715036e3e5212c4d6014c7647f96787f6
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2021-07-08 01:57:30 +0800

    added lower buttons and images

--- architect-portfolio-v1/src/components/Hero.js
+++ architect-portfolio-v1/src/components/Hero.js
@@ -26,9 +26,38 @@ const HeroSlider = styled.div``;
 const HeroImage = styled.img``;
 const HeroContent = styled.div``;
 const Arrow = styled(IoMdArrowRoundForward)``;
-const SliderButtons = styled.div``;
-const PrevArrow = styled(IoArrowBack)``;
-const NextArrow = styled(IoArrowForward)``;
+const SliderButtons = styled.div`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  display: flex;
+  z-index: 10;
+`;
+
+const arrowButtons = css`
+  width: 50px;
+  height: 50px;
+  color: #fff;
+  cursor: pointer;
+  background: #000d1a;
+  border-radius: 50px;
+  padding: 10px;
+  margin-right: 1rem;
+  user-select: none;
+  transition: 0.3s;
+
+  &:hover {
+    background: #193c52;
+    transform: scale(1.05);
+  }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+  ${arrowButtons}
+`;
+const NextArrow = styled(IoArrowForward)`
+  ${arrowButtons}
+`;
 
 const Hero = ({ slides }) => {
   return (
@@ -39,7 +68,7 @@ const Hero = ({ slides }) => {
             return (
               <HeroSlide key={index}>
                 <HeroSlider>
-                  <HeroImage />
+                  <HeroImage src={slide.image} alt={slide.alt} />
                   <HeroContent>
                     <h1>{slide.title}</h1>
                     <h1>{slide.location}</h1>
